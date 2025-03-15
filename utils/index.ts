bd28bd1245a6b99d9df9361b64b6da0c16056877
Khouchane Mahmoud <97807779+kh-mahmoud@users.noreply.github.com>
2025-03-16 00:48:13 +0100

    Update index.ts

--- utils/index.ts
+++ utils/index.ts
@@ -14,7 +14,7 @@ export const fetchCARS = async (filters:FilterProps) => {
     'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com',
   };
 
-  const url=`https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`
+  const url=`https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&fuel_type=${fuel}`
 
 
   try {
